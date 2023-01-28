bfe32b0504712e12d1d5f020b48aab7ec755df4e
shchetnikovich <shchetnikovich@gmail.com>
2023-01-29 00:17:56 +0300

    Add tap gesture close() to DetailView

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -16,19 +16,15 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         popupView.layer.cornerRadius = 10
+        
+        let gestureRecognizer = UITapGestureRecognizer(     //  Слушаем вьюху, если что close() кидаем
+            target: self,
+            action: #selector(close))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
 
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     // MARK: - Actions
     
@@ -36,4 +32,15 @@ class DetailViewController: UIViewController {
       dismiss(animated: true, completion: nil)
     }
 
+}
+
+
+extension DetailViewController: UIGestureRecognizerDelegate {       //  Фиксируем тап вне pop-up вьюхи
+    
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldReceive touch: UITouch
+    ) -> Bool {
+        return (touch.view === self.view)
+    }
 }
