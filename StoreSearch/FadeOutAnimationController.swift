08def4447a5ffe2cd4ca6649aa1673beec879c6f
shchetnikovich <shchetnikovich@gmail.com>
2023-01-29 18:10:33 +0300

    Add new Fade Out animation to close()

---
+++ StoreSearch/FadeOutAnimationController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class FadeOutAnimationController: NSObject, UIViewControllerAnimatedTransitioning {     //  Fade Out Анимация (Затухающая)
+    
+    func transitionDuration(
+        using transitionContext:
+        UIViewControllerContextTransitioning?
+    ) -> TimeInterval {
+        return 0.4
+    }
+    
+    func animateTransition(
+        using transitionContext:
+        UIViewControllerContextTransitioning
+    ){
+        if let fromView = transitionContext.view(
+            forKey: UITransitionContextViewKey.from) {
+            let time = transitionDuration(using: transitionContext)
+            UIView.animate(
+                withDuration: time,
+                animations: {
+                    fromView.alpha = 0      //  Выкручиваем альфу вьюхи в 0
+                }, completion: { finished in
+                    transitionContext.completeTransition(finished)
+                }
+            )
+        }
+    }
+}
