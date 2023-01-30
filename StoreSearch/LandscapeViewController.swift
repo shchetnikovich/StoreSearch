85fed392aa6f390f4be8fae5b6a1d7b0de81e772
shchetnikovich <shchetnikovich@gmail.com>
2023-01-30 14:09:42 +0300

    Add buttons to LandscapeView

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -7,19 +7,19 @@ class LandscapeViewController: UIViewController {
     
     var searchResults = [SearchResult]()
     
+    private var firstTime = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.removeConstraints(view.constraints)    // Отключаем Auto Layout Remove constraints from main view
         view.translatesAutoresizingMaskIntoConstraints = true   //  Позиционируем и изменяем размеры наших вьюх вручную (удаляем автоматические ограничения AutoresizingMask)
-    
+        
         pageControl.removeConstraints(pageControl.constraints)   // Remove constraints for page control
         pageControl.translatesAutoresizingMaskIntoConstraints = true
         
         scrollView.removeConstraints(scrollView.constraints)    // Remove constraints for scroll view
         scrollView.translatesAutoresizingMaskIntoConstraints = true
-        
-//        scrollView.contentSize = CGSize(width: 1000, height: 1000)
     }
     
     override func viewWillLayoutSubviews() {        //  Впервые появляемся на экране, вручную описываем новый layout LandscapeView
@@ -31,7 +31,70 @@ class LandscapeViewController: UIViewController {
             y: safeFrame.size.height - pageControl.frame.size.height,
             width: safeFrame.size.width,
             height: pageControl.frame.size.height)
+        
+        if firstTime {
+            firstTime = false
+            tileButtons(searchResults)
+        }
     }
     
+    // MARK: - Private Methods
     
+    private func tileButtons(_ searchResults: [SearchResult]) {     //  Вручную указывам размер будущих Grid Tile'ов
+        
+        let itemWidth: CGFloat = 94     //  Выйдет grid 3 row, 6 column
+        let itemHeight: CGFloat = 88
+        var columnsPerPage = 0
+        var rowsPerPage = 0
+        var marginX: CGFloat = 0
+        var marginY: CGFloat = 0
+        let viewWidth = scrollView.bounds.size.width
+        let viewHeight = scrollView.bounds.size.height
+        
+        columnsPerPage = Int(viewWidth / itemWidth)         //  Вычисляем кол-во tail'ов
+        rowsPerPage = Int(viewHeight / itemHeight)
+        
+        marginX = (viewWidth - (CGFloat(columnsPerPage) * itemWidth)) * 0.5     //  Вычисляем отступы по х и у
+        marginY = (viewHeight - (CGFloat(rowsPerPage) * itemHeight)) * 0.5
+        
+        //  Размеры будущих кнопок
+        let buttonWidth: CGFloat = 82
+        let buttonHeight: CGFloat = 82
+        let paddingHorz = (itemWidth - buttonWidth) / 2
+        let paddingVert = (itemHeight - buttonHeight) / 2
+        
+        //  Рисуем кнопки
+        var row = 0
+        var column = 0
+        var x = marginX
+        for (index, result) in searchResults.enumerated() {
+            let button = UIButton(type: .system)
+            button.backgroundColor = UIColor.white
+            button.setTitle("\(index)", for: .normal)
+
+            button.frame = CGRect(  //  Обязательно необходимо установить .frame
+                x: x + paddingHorz,
+                y: marginY + CGFloat(row) * itemHeight + paddingVert,
+                width: buttonWidth,
+                height: buttonHeight)
+            
+            scrollView.addSubview(button)
+
+            row += 1
+            if row == rowsPerPage {
+                row = 0; x += itemWidth; column += 1
+                if column == columnsPerPage {
+                    column = 0; x += marginX * 2
+                }
+            }
+        }
+        
+        // Устанавливаем представление для scrollview.contentSize
+        let buttonsPerPage = columnsPerPage * rowsPerPage
+        let numPages = 1 + (searchResults.count - 1) / buttonsPerPage
+        scrollView.contentSize = CGSize(
+            width: CGFloat(numPages) * viewWidth,
+            height: scrollView.bounds.size.height)
+        print("Number of pages: \(numPages)")
+    }
 }
